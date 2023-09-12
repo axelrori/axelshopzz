5384c4206780cf288a5cbd29e053a83c8d15aa5e
axelrori <axelrori204@gmail.com>
2023-09-12 16:14:53 +0700

    pengerjaan lumayan selesai

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def show_main(request):
+    context = {
+        'appname': 'axelshopzz',
+        'name': 'Serafino Theodore Axel Rori',
+        'class': 'PBP F'
+    }
+
+    return render(request, "main.html", context)
