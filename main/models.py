a02d9ec06b4de8bd26c6639e56d8a86d3efd0ab7
axelrori <axelrori204@gmail.com>
2023-09-19 17:54:21 +0700

    aplikasi tugas 3 selesai sisa readme

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    date_added = models.DateField(auto_now_add=True)
     amount = models.IntegerField()
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
-    date_added = models.DateField(auto_now_add=True)
