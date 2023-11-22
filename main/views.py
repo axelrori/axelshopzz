f52054c7475ec5c3ea2a35dd3acc063d76563281
axelrori <axelrori204@gmail.com>
2023-11-22 10:48:04 +0700

    Tugas 9 Selesai

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import *
 from main.forms import ProductForm
@@ -159,3 +160,22 @@ def delete_product_ajax(request, id):
         product.delete()
         return HttpResponse(b"DELETED", status=200)
     return HttpResponseNotFound()
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Product.objects.create(
+            user = request.user,
+            name = data["name"],
+            price = int(data["price"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
